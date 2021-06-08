393ba5c5a8762d875548319882778f5c5a03371b
Swaroop <swaroop.chakraborty@cerner.com>
2021-06-08 01:02:10 -0400

    commit

--- src/com/org/ai/search/bfs/crawler/WebCrawler.java
+++ src/com/org/ai/search/bfs/crawler/WebCrawler.java
@@ -9,12 +9,13 @@ public class WebCrawler {
 
 	public static void main(String[] args) {
 
-		String firstWebsite = "https://www.udemy.com/";
+		String firstWebsite = "https://www.google.com/";
 		String patterns = "http[s]*://(\\w+\\.)*(\\w+)";
 
-		BFS myCrawler = new BFS(firstWebsite, 26, patterns);
+		BFS myCrawler = new BFS(firstWebsite, 3, patterns);
 
 		try {
+			System.out.println("crawling from...." + firstWebsite);
 			Set<String> urlsCrawled = myCrawler.bfs();
 			System.out.println(urlsCrawled.size() + " web sites crawled!");
 			System.out.println("Here is the list: ");
